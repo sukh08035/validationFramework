b5225c3bcc4217b289d67a62a8be21d5743edfe7
unknown <singhs12@NLM01889595OCCS.nlm.nih.gov>
2015-02-23 10:16:32 -0500

    more changes

--- parent/usedition-jpa-service/src/main/java/gov/nih/nlm/mlb/jpa/service/InitEntityManager.java
+++ parent/usedition-jpa-service/src/main/java/gov/nih/nlm/mlb/jpa/service/InitEntityManager.java
@@ -1,10 +1,104 @@
 package gov.nih.nlm.mlb.jpa.service;
 
-public class InitEntityManager {
+import gov.nih.nlm.mlb.jpa.service.utility.ConfigUtility;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.Properties;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+
+public abstract class InitEntityManager {
+
+	protected static EntityManagerFactory factory;
+	protected EntityManager manager;
+	protected boolean transactionPerOperation = true;
+	protected EntityTransaction tx;
+
+	public InitEntityManager() throws Exception {
+		// created once or if the factory has closed
+		if (factory == null || !factory.isOpen()) {
+			openFactory();
+		}
+
+		// created on each instantiation
+		manager = factory.createEntityManager();
+		tx = manager.getTransaction();
 	}
 
+	public synchronized void openFactory() throws Exception {
+
+		// if factory has not been instantiated or has been closed, open it
+		if (factory == null || !factory.isOpen()) {
+
+			Logger.getLogger(this.getClass()).info(
+					"Setting root service entity manager factory.");
+			Properties config = ConfigUtility.getJpaPropterties();
+			factory =
+					Persistence.createEntityManagerFactory("usedition-jpa-model", config);
+		}
+
+	}
+	public void closeFactory() throws Exception {
+		if (factory.isOpen()) {
+			factory.close();
+		}
+	}
+	
+
+	  public boolean getTransactionPerOperation() {
+	    return transactionPerOperation;
+	  }
+
+
+	  public void setTransactionPerOperation(boolean transactionPerOperation) {
+	    this.transactionPerOperation = transactionPerOperation;
+	  }
+
+
+	  public void beginTransaction() {
+
+	    if (getTransactionPerOperation())
+	      throw new IllegalStateException(
+	          "Error attempting to begin a transaction when using transactions per operation mode.");
+	    else if (tx != null && tx.isActive())
+	      throw new IllegalStateException(
+	          "Error attempting to begin a transaction when there "
+	              + "is already an active transaction");
+	    tx = manager.getTransaction();
+	    tx.begin();
+	  }
+
+	
+	  public void commit() throws Exception {
+
+	    if (getTransactionPerOperation())
+	      throw new IllegalStateException(
+	          "Error attempting to commit a transaction when using transactions per operation mode.");
+	    else if (tx != null && !tx.isActive())
+	      throw new IllegalStateException(
+	          "Error attempting to commit a transaction when there "
+	              + "is no active transaction");
+	    if (tx == null) {
+	      throw new Exception("Attempting to commit a null transaction.");
+	    }
+	    tx.commit();
+	    manager.clear();
+	  }
+	  
+	
+	  public void clear() throws Exception {
+	    manager.clear();
+	  }
+
+	
+	  public void close() throws Exception {
+	    if (manager.isOpen()) {
+	      manager.close();
+	    }
+	  }
+	
 }
--- parent/usedition-jpa-service/src/main/java/gov/nih/nlm/mlb/jpa/service/utility/ConfigUtility.java
+++ parent/usedition-jpa-service/src/main/java/gov/nih/nlm/mlb/jpa/service/utility/ConfigUtility.java
@@ -1,16 +1,36 @@
 package gov.nih.nlm.mlb.jpa.service.utility;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
 import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
 
 
 public class ConfigUtility {
 
 	protected static ResourceBundle config;
+	protected static Properties configProp;
 	public synchronized static ResourceBundle getConfigProperties() throws Exception {
 		if (config == null) {
 			 config=ResourceBundle.getBundle("config");
 		}
 		return config;
 	}
+	  
+	public synchronized static Properties getJpaPropterties() throws Exception {
+		    if (configProp == null) {
+		      String configFileName = "src/main/resources/config.properties";
+		      Logger.getLogger(ConfigUtility.class.getName()).info(
+		          "  run.config = " + configFileName);
+		      configProp = new Properties();
+		      FileReader in = new FileReader(new File(configFileName));
+		      configProp.load(in);
+		      in.close();
+		      Logger.getLogger(ConfigUtility.class).info("  properties = " + config);
+		    }
+		    return configProp;
+		  }
 
 }
