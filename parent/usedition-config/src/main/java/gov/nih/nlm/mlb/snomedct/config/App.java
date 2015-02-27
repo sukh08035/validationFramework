d44c215a571c75f727b96d2221eb95fe5b676ba1
Satwinder Singh <satwinder.singh@mail.nlm.nih.gov>
2015-02-27 14:40:37 -0500

    adding config project

---
+++ parent/usedition-config/src/main/java/gov/nih/nlm/mlb/snomedct/config/App.java
@@ -0,0 +1,13 @@
+package gov.nih.nlm.mlb.snomedct.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
